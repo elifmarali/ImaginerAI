f29c42d29f17fd760c9e06562cb46a1b9739657e
Elif Maralı <71133148+elifmarali@users.noreply.github.com>
2025-02-26 10:42:38 +0300

    Dev - Github icon formatı değiştirildi

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -1,14 +1,13 @@
 "use client";
 import React from "react";
 import Image from "next/image";
-import GitHubIcon from "@/assets/icons/github.svg";
 import TwitterIcon from "@/assets/icons/twitter.svg";
 import styles from "@/components/Footer/styles.module.scss";
 function Footer() {
   return (
     <footer className={styles.footer}>
       <div className={styles.footerContainer}>
-        <Image src={GitHubIcon} alt="GitHub" width={30} height={30} />
+        <Image src="@/assets/icons/github.svg" alt="GitHub" width={30} height={30} />
         <Image src={TwitterIcon} alt="Twitter" width={30} height={30} />
         <div className="text-sm">
           Made by E.M. and,<br />
